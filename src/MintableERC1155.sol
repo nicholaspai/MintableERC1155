291631fe8364b6c1f2d2ad53d78e8e68ee3afd41
nicholaspai <npai.nyc@gmail.com>
2023-03-15 18:02:44 -0400

    add Mintable ERC1155
    Signed-off-by: nicholaspai <npai.nyc@gmail.com>

---
+++ src/MintableERC1155.sol
@@ -0,0 +1,85 @@
+import "@openzeppelin/contracts/token/ERC1155/presets/ERC1155PresetMinterPauser.sol";
+
+pragma solidity ^0.8.0;
+
+contract MintableERC1155 is ERC1155PresetMinterPauser {
+
+    string public baseMetadataURI; //the token metadata URI
+
+    /**
+     * @dev Grants `DEFAULT_ADMIN_ROLE`, `MINTER_ROLE`, and `PAUSER_ROLE` to the account that
+     * deploys the contract.
+     */
+    constructor(string memory _uri) ERC1155PresetMinterPauser(_uri) {
+        baseMetadataURI = _uri;
+    }
+
+    /**
+     * @dev Creates `amount` new tokens for `to`, of token type `id`.
+     *
+     * See {ERC1155-_mint}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `MINTER_ROLE`.
+     */
+    function mint(
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public override onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(hasRole(MINTER_ROLE, _msgSender()), "ERC1155PresetMinterPauser: must have minter role to mint");
+
+        _mint(to, id, amount, data);
+    }
+
+    /**
+     * @dev xref:ROOT:erc1155.adoc#batch-operations[Batched] variant of {mint}.
+     */
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public override onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(hasRole(MINTER_ROLE, _msgSender()), "ERC1155PresetMinterPauser: must have minter role to mint");
+
+        _mintBatch(to, ids, amounts, data);
+    }    /**
+     * @dev Sets a new URI for all token types, by relying on the token type ID
+     * substitution mechanism
+     * https://eips.ethereum.org/EIPS/eip-1155#metadata[defined in the EIP].
+     *
+     * By this mechanism, any occurrence of the `\{id\}` substring in either the
+     * URI or any of the amounts in the JSON file at said URI will be replaced by
+     * clients with the token type ID.
+     *
+     * For example, the `https://token-cdn-domain/\{id\}.json` URI would be
+     * interpreted by clients as
+     * `https://token-cdn-domain/000000000000000000000000000000000000000000000000000000000004cce0.json`
+     * for token type ID 0x4cce0.
+     *
+     * See {uri}.
+     *
+     * Because these URIs cannot be meaningfully represented by the {URI} event,
+     * this function emits no events.
+     */
+    function setURI(string memory newuri) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _setURI(newuri);
+    }
+
+    /*
+    sets our URI and makes the ERC1155 OpenSea compatible
+    */
+    function uri(uint256 _tokenid) override public view returns (string memory) {
+        return string(
+            abi.encodePacked(
+                baseMetadataURI,
+                Strings.toString(_tokenid),".json"
+            )
+        );
+    }
+
+
+}
